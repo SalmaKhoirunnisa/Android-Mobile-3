3974bc2b49d38b2c7827965efc388b4f84e93e02
SalmaKhoirunnisa <121269129+SalmaKhoirunnisa@users.noreply.github.com>
2023-09-29 11:24:30 +0530

    Create SecondActivity.java

---
+++ SecondActivity.java
@@ -0,0 +1,12 @@
+package iat.pam.intentproject;
+
+import android.app.Activity;
+import android.view.View;
+
+public class SecondActivity extends Activity {
+    public static final String EXTRA_REPLY = "com.example.android.twoactivities.extra.REPLY";
+
+    public void returnReply(View view) {
+        // Your code here
+    }
+}
